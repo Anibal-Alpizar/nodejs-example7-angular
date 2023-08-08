51bfc173f0da8a2656874ed06f047d019d34cc39
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-08-08 14:32:30 -0600

    repro

--- server/controllers/ordenController.js
+++ server/controllers/ordenController.js
@@ -1,4 +1,4 @@
-const { PrismaClient, Prisma } = require("@prisma/client");
+const { PrismaClient, Prisma } = require('@prisma/client');
 const prisma = new PrismaClient();
 //Obtener listado
 module.exports.get = async (request, response, next) => {
@@ -31,30 +31,36 @@ module.exports.getById = async (request, response, next) => {
 };
 //Crear
 module.exports.create = async (request, response, next) => {
-  let infoOrden=request.body;
-  const newVideojuego =await prisma.orden.create({
-    data:{
-      fechaOrden:infoOrden.fechaOrden,
-      usuarioId:1,
-      videojuegos:{
-        createMany:{
+  let infoOrden = request.body;
+  const newVideojuego = await prisma.orden.create({
+    data: {
+      fechaOrden: infoOrden.fechaOrden,
+      usuarioId: 1,
+      videojuegos: {
+        createMany: {
           //[{videojuegoId, cantidad}]
-          data: infoOrden.videojuegos
-        }
-      }
-    }   
-  })
-  response.json(newVideojuego)
+          data: infoOrden.videojuegos,
+        },
+      },
+    },
+  });
+  response.json(newVideojuego);
 };
 module.exports.getVentaProductoMes = async (request, response, next) => {
-  let mes = parseInt(request.params.mes); 
- 
+  let mes = parseInt(request.params.mes);
+
   //SELECT v.nombre, SUM(ov.cantidad) as suma FROM orden o, ordenonvideojuego ov, videojuego v WHERE o.id=ov.ordenId and ov.videojuegoId=v.id AND MONTH(o.fechaOrden) = 10 GROUP BY ov.videojuegoId
-  
+  const result = await prisma.$queryRaw(
+    Prisma.sql`SELECT v.nombre, SUM(ov.cantidad) as suma FROM orden o, ordenonvideojuego ov, videojuego v WHERE o.id=ov.ordenId and ov.videojuegoId=v.id AND MONTH(o.fechaOrden) = ${mes} GROUP BY ov.videojuegoId`
+  );
+  response.json(result);
 };
 module.exports.getVentaProductoTop = async (request, response, next) => {
-  let mes = parseInt(request.params.mes); 
- 
+  let mes = parseInt(request.params.mes);
+
   //SELECT v.nombre, (SUM(ov.cantidad)*v.precio) as total FROM orden o, ordenonvideojuego ov, videojuego v WHERE o.id=ov.ordenId and ov.videojuegoId=v.id GROUP BY ov.videojuegoId ORDER BY total DESC;
-  
+  const result = await prisma.$queryRaw(
+    Prisma.sql`SELECT v.nombre, (SUM(ov.cantidad)*v.precio) as total FROM orden o, ordenonvideojuego ov, videojuego v WHERE o.id=ov.ordenId and ov.videojuegoId=v.id GROUP BY ov.videojuegoId ORDER BY total DESC;`
+  );
+  response.json(result);
 };
