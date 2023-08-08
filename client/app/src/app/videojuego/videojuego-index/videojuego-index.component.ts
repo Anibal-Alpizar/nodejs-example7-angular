1d78eb41b78587d968e0f29b988f785a7666639e
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-08-08 13:59:45 -0600

    filters

--- client/app/src/app/videojuego/videojuego-index/videojuego-index.component.ts
+++ client/app/src/app/videojuego/videojuego-index/videojuego-index.component.ts
@@ -1,60 +1,77 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, filter, takeUntil } from 'rxjs';
 import { GenericService } from 'src/app/share/generic.service';
 import { VideojuegoDiagComponent } from '../videojuego-diag/videojuego-diag.component';
 import { CartService } from 'src/app/share/cart.service';
-import { NotificacionService, TipoMessage } from 'src/app/share/notification.service';
+import {
+  NotificacionService,
+  TipoMessage,
+} from 'src/app/share/notification.service';
 
 @Component({
   selector: 'app-videojuego-index',
   templateUrl: './videojuego-index.component.html',
-  styleUrls: ['./videojuego-index.component.css']
+  styleUrls: ['./videojuego-index.component.css'],
 })
 export class VideojuegoIndexComponent {
-  datos:any;//Respuesta del API
-  destroy$:Subject<boolean>=new Subject<boolean>();
+  datos: any; //Respuesta del API
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  filterDatos: any;
 
-  constructor(private gService:GenericService,
-    private dialog:MatDialog,
-    private cartService:CartService,
-    private notificacion:NotificacionService
-    ){
-    this.listaVideojuegos(); 
+  constructor(
+    private gService: GenericService,
+    private dialog: MatDialog,
+    private cartService: CartService,
+    private notificacion: NotificacionService
+  ) {
+    this.listaVideojuegos();
   }
   //Listar los videojuegos llamando al API
-  listaVideojuegos(){
+  listaVideojuegos() {
     //localhost:3000/videojuego
-    this.gService.list('videojuego/')
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((data:any)=>{
-        console.log(data);
-        this.datos=data;
-      });
-    
-  }
-  detalleVideojuego(id:number){
-    const dialogConfig=new MatDialogConfig();
-    dialogConfig.disableClose=false;
-    dialogConfig.data={
-      id:id
-    };
-    this.dialog.open(VideojuegoDiagComponent,dialogConfig);
-  }
-  comprar(id:number){
     this.gService
-    .get('videojuego',id)
-    .pipe(takeUntil(this.destroy$))
-    .subscribe((data:any)=>{
-      //Agregar videojuego obtenido del API al carrito
-      this.cartService.addToCart(data);
-      //Notificar al usuario
-      this.notificacion.mensaje(
-        'Orden',
-        'Videojuego: '+data.nombre+ ' agregado a la orden',
-        TipoMessage.success
-      )
-    });
+      .list('videojuego/')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: any) => {
+        console.log(data);
+        this.datos = data;
+        this.filterDatos = this.datos;
+      });
+  }
+
+  filterVideoJuegos(text: string) {
+    if (!text) {
+      this.filterDatos = this.datos;
+    } else {
+      this.filterDatos = this.datos.filter((videojuego) =>
+        videojuego?.nombre.toLowerCase().includes(text.toLowerCase())
+      );
+    }
+  }
+
+  detalleVideojuego(id: number) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.data = {
+      id: id,
+    };
+    this.dialog.open(VideojuegoDiagComponent, dialogConfig);
+  }
+  comprar(id: number) {
+    this.gService
+      .get('videojuego', id)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: any) => {
+        //Agregar videojuego obtenido del API al carrito
+        this.cartService.addToCart(data);
+        //Notificar al usuario
+        this.notificacion.mensaje(
+          'Orden',
+          'Videojuego: ' + data.nombre + ' agregado a la orden',
+          TipoMessage.success
+        );
+      });
   }
   ngOnDestroy() {
     this.destroy$.next(true);
